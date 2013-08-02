44666421ceaa7964c7e6b73bd1e4c451a019cee8
akash <akash0x53s@gmail.com>
2013-08-02 23:54:55 +0530

    worker added

--- main.py
+++ main.py
@@ -4,51 +4,26 @@ import urllib2 as ulib
 from time import sleep
 from md5 import md5
 import sys
-
-def get_hash (link):
-	try:
-
-		page=ulib.urlopen(link)
-		fetch_content_length=page.headers.headers[len(page.headers.headers)-1]
-		#print fetch_content_length
-		hash_code=md5(fetch_content_length).hexdigest()
-		return hash_code
-
-	except:
-		print 'Error: could not open the page'
-
-
-if len(sys.argv)<=1 :
-	print 'Usage: notifier <weg page link>'
-	sys.exit(1)
-
+from sms import send
+from notifier import GenHash
 
 #dummy_link="http://www.unipune.ac.in/university_files/results.htm"
-link=sys.argv[1]
-initial_hash=get_hash(link)
-flag=False
-print initial_hash
+__initial_hash__=""
 
 if __name__=="__main__" :
 	__version__=1.0
-	__name__="Notifier"
 	__data__="Notifier, is an app, that notifies you via SMS whenever change occurred on given webpage."
-
-
-
-	print 'main started'
-
-
-	while True:
-		print "latest hash", get_hash(link)
-		if initial_hash!=get_hash(link):
-			print "Content changed. Sending SMS."
-			break
-		sleep(5)
-
-
 	
-
-
-
+	if len(sys.argv)<=1 :
+		print 'Usage: notifier <weg page link>'
+		sys.exit(1)
+	
+	worker=GenHash(sys.argv[1])
+	__initial_hash__=worker.initial_hash
+	print 'initial hash',__initial_hash__
+	
+	while True:
+		print worker.header_hash()
+		sleep(5)
+	
 	
